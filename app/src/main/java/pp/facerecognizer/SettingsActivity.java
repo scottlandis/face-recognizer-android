3e1e49346a6faa92d24295088a132e39d1d9435c
jvinall <jvinall@augmentedengagement.com>
2019-03-18 15:43:46 -0700

    Added SOS library capability

---
+++ app/src/main/java/pp/facerecognizer/SettingsActivity.java
@@ -0,0 +1,23 @@
+package pp.facerecognizer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+public class SettingsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new PrefsFragment()).commit();
+    }
+
+    public static class PrefsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.prefs_app);
+        }
+    }
+}
